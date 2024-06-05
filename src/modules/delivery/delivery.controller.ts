d1ed99dd12398ab09b6f04d096066ea150cfb8f5
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-06-05 19:29:52 +0600

    Working with delivery 1

--- src/modules/delivery/delivery.controller.ts
+++ src/modules/delivery/delivery.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import { Delivery } from './models/delivery.model';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('deliveries')
+@ApiTags('Deliveries')
 export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) {}
 
