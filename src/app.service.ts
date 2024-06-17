99828f4fdaac947cbcbdb7dc4f4eae0eb335f6e9
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-06-18 02:57:51 +0600

    mysql query added in app service to stop sleeping the connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectEntityManager() private readonly entityManager: EntityManager,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getHelloAsync(): Promise<string> {
+    const rawQuery = `
+        SELECT COUNT(*) AS total_riders
+        FROM riders;
+        `;
+
+    const result = await this.entityManager.query(rawQuery);
+
+    if (result.length === 0) {
+      return null;
+    }
+    return 'Total riders: ' + result[0].total_riders;
   }
 }
