4b182bcbd44ff64668d0bc47537dc0fe94e06575
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-07-14 18:03:49 +0600

    working with post api

---
+++ src/modules/payments/controllers/payment-method.controller.ts
@@ -0,0 +1,95 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Body,
+  Res,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { PaymentMethodService } from '../services/payment-method.service';
+import { Response } from 'express';
+import { AddPaymentMethodDto } from '../dtos/add-payment-method.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '@core/guards/jwt-auth.guard';
+
+@Controller('payment-method')
+@ApiTags('Payment Method')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access_token')
+export class PaymentMethodController {
+  constructor(private readonly paymentMethodService: PaymentMethodService) {}
+  //! add payment method
+  @Post('')
+  @ApiOperation({
+    summary: 'Add a new payment method and attach it to customer',
+  })
+  @ApiBody({ type: AddPaymentMethodDto })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        pmID: {
+          type: 'string',
+          example: 'pm_1PYo08Hypixj3Ac96ooIWK6j',
+        },
+        isDefault: { type: 'boolean', example: true },
+      },
+    },
+  })
+  async addPaymentMethod(
+    @Res() res: Response,
+    @Body() body: AddPaymentMethodDto,
+  ): Promise<any> {
+    const { pmID, isDefault } = body;
+    const result: any = await this.paymentMethodService.addPaymentMethod({
+      pmID,
+      isDefault,
+    });
+
+    return res.status(200).json({
+      status: result.status,
+      message: result.message,
+      data: result.data,
+    });
+  }
+
+  //! get all payment method by user
+  @Get('get-all-by-user')
+  @ApiOperation({
+    summary: 'Get all payment methods by user',
+  })
+  async getAllPaymentMethodByUser(@Res() res: Response) {
+    const result = await this.paymentMethodService.getAllPaymentMethodByUser();
+    return res.status(200).json({
+      status: result.status,
+      message: result.message,
+      data: result.data,
+    });
+  }
+  //! delete payment method
+  @Delete(':pmID')
+  @ApiOperation({
+    summary: 'Delete payment method',
+  })
+  @ApiParam({
+    name: 'pmID',
+    description: 'The Payment Method ID for to be deleted',
+  })
+  async deletePaymentMethod(@Res() res: Response, @Param('pmID') pmID: string) {
+    const result = await this.paymentMethodService.deletePaymentMethod(pmID);
+    return res.json({
+      status: result.status,
+      message: result.message,
+      data: result.data,
+    });
+  }
+}
