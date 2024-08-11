95b1b8773921d3b077bcf5e564cced6f99330115
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-08-11 18:10:25 +0600

    Deploy

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /tradebar-rider-nest-api
+cd /tradebar-rider-api-dev
 git pull origin main
 npm install
 pm2 stop all
-pm2 start dist/main.js --name tradebar-rider-nest-api
+pm2 start dist/main.js --name tradebar-rider-api-dev
 pm2 save
